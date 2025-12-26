81d63e381dadec3b141510b42f7184eeb9c1c745
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-12-26 23:08:17 +0900

    💄 체크박스 커스텀

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,8 +1,9 @@
 import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-import { CheckIcon } from "lucide-react"
+
 
 import { cn } from "@/lib/utils"
+import CheckIcon from "@/component/Icon/CheckIcon"
 
 function Checkbox({
   className,
@@ -12,16 +13,16 @@ function Checkbox({
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer border-input dark:bg-input/30 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "peer border-primary-0 dark:bg-input/30 data-[state=checked]:bg-primary-10 data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary-0 focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-[18px] shrink-0 rounded-[4px] border transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
     >
       <CheckboxPrimitive.Indicator
         data-slot="checkbox-indicator"
-        className="grid place-content-center text-current transition-none"
+        className="place-content-center text-current transition-none flex items-center justify-center"
       >
-        <CheckIcon className="size-3.5" />
+        <CheckIcon size={18} className="text-primary-0" />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   )
