3180f3865b1bd99b8a204acad0aa8f12bb6cf14a
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-12-28 15:26:03 +0900

    ✨ 시간 형식 개선: Timer 컴포넌트에서 시간을 두 자리로 포맷하는 기능 추가

--- src/component/Timer/Timer.tsx
+++ src/component/Timer/Timer.tsx
@@ -8,20 +8,24 @@ interface TimerProps {
 }
 
 const Timer = ({ hours, minutes, seconds }: TimerProps) => {
+  const formatTime = (time: number) => {
+    return time.toString().padStart(2, "0");
+  };
+
   return (
     <div className="flex items-center justify-center gap-2">
       <div className="border-primary-0 bg-linear text-primary-0 flex h-full w-full flex-col items-center justify-center rounded-xl border-2 px-2 pt-2 pb-9 text-[154px]">
-        {hours}
+        {formatTime(hours)}
         <span className="typography-body-b">HOURS</span>
       </div>
       <ColonIcon className="text-primary-0 mx-12" />
       <div className="border-primary-0 bg-linear text-primary-0 flex h-full w-full flex-col items-center justify-center rounded-xl border-2 px-2 pt-2 pb-9 text-[154px]">
-        {minutes}
+        {formatTime(minutes)}
         <span className="typography-body-b">MINUTES</span>
       </div>
       <ColonIcon className="text-primary-0 mx-12" />
       <div className="border-primary-0 bg-linear text-primary-0 flex h-full w-full flex-col items-center justify-center rounded-xl border-2 px-2 pt-2 pb-9 text-[154px]">
-        {seconds}
+        {formatTime(seconds)}
         <span className="typography-body-b">SECONDS</span>
       </div>
     </div>
