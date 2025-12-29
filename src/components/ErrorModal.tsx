f76b245317696a204d42e3c5f9c668f045d11f71
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-12-29 17:56:07 +0900

    ♻️ 다이얼로그 컴포넌트 리팩토링

--- src/components/ErrorModal.tsx
+++ src/components/ErrorModal.tsx
@@ -1,12 +1,4 @@
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from "./ui/dialog";
-import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { CustomDialog } from "./Dialog/CustomDialog";
 
 interface ErrorModalProps {
   open: boolean;
@@ -22,16 +14,18 @@ export const ErrorModal = ({
   onOpenChange,
 }: ErrorModalProps) => {
   return (
-    <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent showCloseButton={false} className="sm:max-w-md">
-        <VisuallyHidden>
-          <DialogTitle>{title}</DialogTitle>
-        </VisuallyHidden>
-        <DialogHeader>
-          <DialogTitle>{title}</DialogTitle>
-          <DialogDescription>{description}</DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
+    <CustomDialog open={open} onOpenChange={onOpenChange}>
+      <CustomDialog.Content>
+        <CustomDialog.Header>
+          <CustomDialog.Title>{title}</CustomDialog.Title>
+          <CustomDialog.Description>{description}</CustomDialog.Description>
+        </CustomDialog.Header>
+        <CustomDialog.Footer>
+          <CustomDialog.ConfirmButton className="w-full">
+            확인
+          </CustomDialog.ConfirmButton>
+        </CustomDialog.Footer>
+      </CustomDialog.Content>
+    </CustomDialog>
   );
 };
